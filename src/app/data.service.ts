e8b1aa01de7b0e11549373fc91cbcbdab105959f
Raeceph <cephy88@gmail.com>
2018-02-28 23:32:36 +0800

    data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HTTP} from '@angular/http';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-  constructor() { }
+result:any;
+
+  constructor(private _http: Http) { }
+
+  getPrices(){
+
+retuen this._http.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,IOT&tsyms=USD')
+.map(result => this.result = result.json())
+
+  }
 
 }
